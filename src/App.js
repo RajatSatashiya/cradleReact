091041ec89daa6190dabc3e0b530dbd6abda24b2
Rajat Satashiya <kshri4398@gmail.com>
2021-11-11 01:54:44 +0530

    deployment#5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Signup from "./components/Signup";
 function App() {
   return (
     <>
-      <Router>
+      <Router basename="/">
         <Switch>
           <Route path="/" exact component={Login} />
 
